67970ea4eb037bb21d0d00317260ce6323350d0f
Mykhailo Hurin <mykhailo.hurin@p2h.com>
2022-10-14 17:37:20 +0300

    fix(script): add swipe listeners

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let scoreBlock;
 let frameId = null;
 let score = 0;
 let isGamePaused = false;
-const snakeSpeedByDefault = 4;
+const snakeSpeedByDefault = 5;
 
 const config = {
     step: 0,
@@ -58,7 +58,7 @@ function startGame(isNew = false) {
     randomPositionBerry();
     if (frameId) cancelAnimationFrame(frameId);
     frameId = requestAnimationFrame( gameLoop );
-};
+}
 startGame(true);
 
 function pauseGame() {
@@ -80,7 +80,7 @@ function drawSnake() {
     }
 
     snake.tails.forEach( function(el, index){
-        if (index == 0) {
+        if (!index) {
             context.fillStyle = "#FA0556";
         } else {
             context.fillStyle = "#A00034";
@@ -96,7 +96,7 @@ function drawSnake() {
         for( let i = index + 2; i < snake.tails.length; i++ ) {
 
             if ( el.x == snake.tails[i].x && el.y == snake.tails[i].y ) {
-                refreshGame(false);
+                refreshGame();
             }
 
         }
@@ -117,7 +117,7 @@ function collisionBorder() {
         snake.y = 0;
     }
 }
-function refreshGame(newGame = true) {
+function refreshGame() {
     snake.x = 160;
     snake.y = 160 ;
     snake.tails = [];
@@ -153,27 +153,86 @@ function getRandomInt(min, max) {
     return Math.floor( Math.random() * (max - min) + min );
 }
 
+function moveUp(head, neck) {
+    if (head.y !== neck.y ) return;
+    snake.dy = -config.sizeCell;
+    snake.dx = 0;
+}
+function moveDown(head, neck) {
+    if (head.y !== neck.y ) return;
+    snake.dy = config.sizeCell;
+    snake.dx = 0;
+}
+function moveRight(head, neck) {
+    if (head.y === neck.y ) return;
+    snake.dx = config.sizeCell;
+    snake.dy = 0;
+}
+function moveLeft(head, neck) {
+    if (head.y === neck.y ) return;
+    snake.dx = -config.sizeCell;
+    snake.dy = 0;
+}
+
 document.addEventListener("keydown", function (e) {
     let head = snake.tails[0];
     let neck = snake.tails[1];
     if ( !isGamePaused && KeysCombinations.Up.includes(e.code) ) {
-        if (head.y !== neck.y ) return;
-        snake.dy = -config.sizeCell;
-        snake.dx = 0;
+        moveUp(head, neck);
     } else if ( !isGamePaused && KeysCombinations.Left.includes(e.code) ) {
-        if (head.y === neck.y ) return;
-        snake.dx = -config.sizeCell;
-        snake.dy = 0;
+        moveLeft(head, neck);
     } else if ( !isGamePaused && KeysCombinations.Down.includes(e.code) ) {
-        if (head.y !== neck.y ) return;
-        snake.dy = config.sizeCell;
-        snake.dx = 0;
+        moveDown(head, neck);
     } else if ( !isGamePaused && KeysCombinations.Right.includes(e.code) ) {
-        if (head.y === neck.y ) return;
-        snake.dx = config.sizeCell;
-        snake.dy = 0;
+        moveRight(head, neck);
     } else if ( KeysCombinations.Pause.includes(e.code) ) {
         pauseGame();
     }
-
 });
+
+
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+
+let xDown = null;
+let yDown = null;
+
+function getTouches(evt) {
+    return evt.touches ||             // browser API
+        evt.originalEvent.touches; // jQuery
+}
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+}
+
+function handleTouchMove(evt) {
+    let head = snake.tails[0];
+    let neck = snake.tails[1];
+    if ( ! xDown || ! yDown ) return;
+
+    let xUp = evt.touches[0].clientX;
+    let yUp = evt.touches[0].clientY;
+
+    let xDiff = xDown - xUp;
+    let yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            moveLeft(head, neck);
+        } else {
+            moveRight(head, neck);
+        }
+    } else {
+        if ( yDiff > 0 ) {
+            moveUp(head, neck);
+        } else {
+            moveDown(head, neck);
+        }
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
+}
